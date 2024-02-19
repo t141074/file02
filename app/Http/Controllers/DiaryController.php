7ae93fce5083a21dd635c48c6f4ab53fc11b33e7
t141074 <t141074@wakayama-u.ac.jp>
2024-02-19 16:12:02 +0900

    新しい環境に移行するた ; git commit -m 新しい環境に移行するため . git add ../.c9/metadata/environment/blog/resources/views/diary/ x git add . git commit -m 新しい環境に移行するた

--- app/Http/Controllers/DiaryController.php
+++ app/Http/Controllers/DiaryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Diary;
+use Illuminate\Support\Facades\View;
 
 class DiaryController extends Controller
 {
@@ -24,6 +25,13 @@ class DiaryController extends Controller
 
     public function translate(Request $request)
     {
-        // 翻訳ロジックをここに追加する
+            // 日本語のテキストを取得
+    $japaneseText = $request->input('japanese_text');
+
+    // OpenAIを使用して日本語テキストを英文に変換する処理
+    $englishText = "Translated English Text"; // ここに実際の翻訳処理を記述する
+
+    // ビューにデータを渡す
+    return View::make('diary.translate', compact('japaneseText', 'englishText'));
     }
 }
